73e205553da670a104dbf30d16f8dca73fb3bf08
Chris Nightingale <chrisdruidman@gmail.com>
2025-05-06 19:37:43 +0100

    Update .gitignore to ignore launcher output log and modify start-launcher.bat to capture output

--- osrs-helper-launcher/start-launcher.bat
+++ osrs-helper-launcher/start-launcher.bat
@@ -1,9 +1,12 @@
-@echo off
+@echo on
 setlocal
 REM Build the launcher (optional, comment out if already built)
 mvn -q -f "%~dp0pom.xml" package
 
 REM Run the launcher
-java -jar target\osrs-helper-launcher-1.0-SNAPSHOT.jar
+java -jar target\osrs-helper-launcher-1.0-SNAPSHOT.jar > launcher-output.txt 2>&1
+
+type launcher-output.txt
+pause
 
 endlocal
