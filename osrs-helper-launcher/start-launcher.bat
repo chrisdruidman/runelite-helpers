3f4b1df7ca66f553723446ec503067b3475d5ce1
Chris Nightingale <chrisdruidman@gmail.com>
2025-05-06 19:39:30 +0100

    Add Maven Shade Plugin for creating an executable JAR and update start-launcher.bat to suppress command output

--- osrs-helper-launcher/start-launcher.bat
+++ osrs-helper-launcher/start-launcher.bat
@@ -1,7 +1,7 @@
-@echo on
+@echo off
 setlocal
 REM Build the launcher (optional, comment out if already built)
-mvn -q -f "%~dp0pom.xml" package
+mvn -f "%~dp0pom.xml" package
 
 REM Run the launcher
 java -jar target\osrs-helper-launcher-1.0-SNAPSHOT.jar > launcher-output.txt 2>&1
