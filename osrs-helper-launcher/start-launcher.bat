e8d63b62e0a974be36f5539899f1a65236331b2c
Chris Nightingale <chrisdruidman@gmail.com>
2025-05-06 19:59:35 +0100

    Add maven.compiler.release property to pom.xml and update start-launcher.bat to specify Java path

--- osrs-helper-launcher/start-launcher.bat
+++ osrs-helper-launcher/start-launcher.bat
@@ -4,7 +4,7 @@ REM Build the launcher (optional, comment out if already built)
 mvn -f "%~dp0pom.xml" package
 
 REM Run the launcher
-java -jar target\osrs-helper-launcher-1.0-SNAPSHOT.jar > launcher-output.txt 2>&1
+"C:\Users\loomy\.jdks\corretto-11.0.26\bin\java" -jar target\osrs-helper-launcher-1.0-SNAPSHOT.jar -jar target\osrs-helper-launcher-1.0-SNAPSHOT.jar > launcher-output.txt 2>&1
 
 type launcher-output.txt
 pause
